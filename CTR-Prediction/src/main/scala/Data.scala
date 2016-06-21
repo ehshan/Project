e2cc47470ffeec78bbb29c1f8f7d77914880a24c
Ehshan Veerabangsa <ehshan@msn.com>
2016-06-21 20:56:49 +0100

    updated buildSchema

--- CTR-Prediction/src/main/scala/Data.scala
+++ CTR-Prediction/src/main/scala/Data.scala
@@ -12,14 +12,14 @@ object Data {
   //location of the data-change as appropriate
   val path = "E:\\_MSC_PROJECT\\datasets\\ipinyou-dataset\\ipinyou.contest.dataset"
 
-  def main (args: Array[String]){}
+  def main (args: Array[String]){
+)
+  }
 
-  def buildSchema(file: File): StructType = {
+  def buildSchema(file: String): StructType = {
     var schemaString = ""
 
-    val filename = ".\\.\\.\\.\\schema"
-    for (line <- Source.fromFile(filename).getLines) schemaString = schemaString + line+" "
-
+    for (line <- Source.fromFile(file).getLines) schemaString = schemaString + line+" "
     //create header for data-frame using column objects based on schema
     StructType(schemaString.split(" ").map(fieldName ⇒ StructField(fieldName, StringType, nullable = true)))
   }
