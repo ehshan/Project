168d8a84f7f1d4eac672805b66ed6f8cc6924032
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-12 19:26:30 +0100

    moved analysis methods to new object

--- CTR-Prediction/src/main/scala/Analysis.scala
+++ CTR-Prediction/src/main/scala/Analysis.scala
@@ -1,5 +1,36 @@
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
 object Analysis {
 
+  def targetFeatures(df: DataFrame) {
 
+    //count number of records
+    val allRecords = df.count()
+    println("Total no records: " + allRecords)
+
+    //Average click-through rate for all records
+    val avgCTR = df.select(avg("Click")).cache()
+    avgCTR.show()
+  }
+
+
+  def distinctValues(df: DataFrame) {
+    //TODO find faster way to do this
+    val allCounts = df.select(
+      countDistinct("BidID"), countDistinct("iPinYouID"), countDistinct("UserAgent"),
+      countDistinct("IP"), countDistinct("Region"), countDistinct("City"),
+      countDistinct("AdExchange"), countDistinct("Domain"), countDistinct("URL"),
+      countDistinct("AnonymousURLID"), countDistinct("AdSlotID"), countDistinct("AdSlotWidth"),
+      countDistinct("AdSlotHeight"), countDistinct("AdSlotVisibility"), countDistinct("AdSlotFormat"),
+      countDistinct("AdSlotFloorPrice"), countDistinct("CreativeID"), countDistinct("BiddingPrice"),
+      countDistinct("PayingPrice"), countDistinct("KeyPageURL"), countDistinct("AdvertiserID"),
+      countDistinct("UserTags"), countDistinct("Click"), countDistinct("Year"),
+      countDistinct("Month"), countDistinct("Day"), countDistinct("Hour"),
+      countDistinct("TimeOfDay")
+    ).cache()
+    allCounts.show()
+
+  }
 
 }
--- CTR-Prediction/src/main/scala/Data.scala
+++ CTR-Prediction/src/main/scala/Data.scala
@@ -2,7 +2,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 
 import org.apache.spark.sql.{DataFrame, SQLContext}
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.sql.functions._
 
@@ -183,35 +183,6 @@ object Data {
     showFrame(option)
   }
 
-  def targetFeatures(df: DataFrame) {
-
-    //count number of records
-    val allRecords = df.count()
-    println("Total no records: " + allRecords)
-
-    //Average click-through rate for all records
-    val avgCTR = df.select(avg("Click")).cache()
-    avgCTR.show()
-  }
-
-
-  def distinctValues(df: DataFrame) {
-    //TODO find faster way to do this
-    val allCounts = df.select(
-      countDistinct("BidID"), countDistinct("iPinYouID"), countDistinct("UserAgent"),
-      countDistinct("IP"), countDistinct("Region"), countDistinct("City"),
-      countDistinct("AdExchange"), countDistinct("Domain"), countDistinct("URL"),
-      countDistinct("AnonymousURLID"), countDistinct("AdSlotID"), countDistinct("AdSlotWidth"),
-      countDistinct("AdSlotHeight"), countDistinct("AdSlotVisibility"), countDistinct("AdSlotFormat"),
-      countDistinct("AdSlotFloorPrice"), countDistinct("CreativeID"), countDistinct("BiddingPrice"),
-      countDistinct("PayingPrice"), countDistinct("KeyPageURL"), countDistinct("AdvertiserID"),
-      countDistinct("UserTags"), countDistinct("Click"), countDistinct("Year"),
-      countDistinct("Month"), countDistinct("Day"), countDistinct("Hour"),
-      countDistinct("TimeOfDay")
-    ).cache()
-    allCounts.show()
-
-  }
 }
 /**
   * Object to override the initial date format for a calendar object
