a72c0fdc4cbe86369f2567e4a33a1a4c3a72c191
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-20 18:43:52 +0100

    moved methods from Data to Store

--- CTR-Prediction/src/main/scala/Data.scala
+++ CTR-Prediction/src/main/scala/Data.scala
@@ -151,70 +151,7 @@ object Data {
 
     df.join(imps,"iPinYouID")
   }
-
-  def splitByAdvertiser(df: DataFrame, sQLContext: SQLContext): Map[Any, DataFrame] = {
-    //TODO instantiate single instances of SQL and Spark Contexts
-    import sQLContext.implicits._
-
-    //sequence of advertisers
-    val advertisers = df.select("AdvertiserID").distinct.collect.flatMap(_.toSeq)
-
-    //map containing all df's split by AdvertiserID - keys should be IDs
-    val dfMap = advertisers.map(advertiser => advertiser -> df.where($"AdvertiserID" <=> advertiser)).toMap
-
-    dfMap
-  }
-
-  /**
-    * Writes the data-set to individual csv files - split by advertiser
-    *
-    * @param map
-    */
-  def saveByAdvertiser(map: Map[Any, DataFrame]){
-    map.values.foreach(i =>
-      map(i).write.format("com.databricks.spark.csv").option("header", "true")
-        .save(path+"\\"+i)
-    )
-  }
-
-  /**
-    *
-    * @param id
-    * @param map
-    * @return
-    */
-  def getFrameByAdvertiser(id: String, map: Map[Any,DataFrame]): DataFrame = {
-    val option = map.get(id)
-
-    /**
-      * Helper method to unpack option
-      *
-      * @param opt
-      * @return
-      */
-    def showFrame(opt: Option[DataFrame]) =
-      opt match {
-        case Some(s) => s
-        case None => null
-      }
-    showFrame(option)
-  }
-
-  /**
-    * Method to get single data-frame for algorithm  testing
-    *
-    * @param sc
-    * @param sqlContext
-    * @return
-    */
-  def getSingleFrame(sc: SparkContext, sqlContext: SQLContext): DataFrame ={
-    val path = "D:\\_MSC_PROJECT\\written-data\\written-set\\3427.csv"
-
-    val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true")
-      .load(path)
-    df
-  }
-
+  
 }
 /**
   * Object to override the initial date format for a calendar object
