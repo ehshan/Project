4663752c608d07c6be5493e7944de53695e7b148
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-22 18:17:54 +0100

    created new run object with main method

---
+++ CTR-Prediction/src/main/scala/Run.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by Ehshan on 21/07/2016.
+  */
+object Run {
+
+  def main(args: Array[String]){
+
+  }
+}
