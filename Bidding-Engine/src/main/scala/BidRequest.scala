f07d4c107e93ab172169904559c9bab3f3dd24fa
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-25 19:29:13 +0100

    updated BidRequest with constructor

--- Bidding-Engine/src/main/scala/BidRequest.scala
+++ Bidding-Engine/src/main/scala/BidRequest.scala
@@ -23,9 +23,33 @@ case class BidRequest (
                   val userTags: String
                 )
 
-object BidRequest{
+object BidRequest {
 
-  def apply():BidRequest={
-    new BidRequest
+  def apply(request: String): BidRequest = {
+    val feature = request.split("\t")
+
+    new BidRequest(
+      feature(0),
+      feature(1),
+      feature(2),
+      feature(3),
+      feature(4),
+      feature(5),
+      feature(6),
+      feature(7),
+      feature(8),
+      feature(9),
+      feature(10),
+      feature(11),
+      feature(12),
+      feature(13),
+      feature(14),
+      feature(15),
+      feature(16),
+      feature(17),
+      feature(18).toInt,
+      feature(19),
+      feature(20)
+    )
   }
 }
