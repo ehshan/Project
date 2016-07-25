9ca420436a50d4806c9cc2e33fc592a0b98fcc4a
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-25 19:12:12 +0100

    added parameter to BidRequest

--- Bidding-Engine/src/main/scala/BidRequest.scala
+++ Bidding-Engine/src/main/scala/BidRequest.scala
@@ -1,7 +1,27 @@
 
-case class BidRequest {
-
-}
+case class BidRequest (
+                  val bidId: String,
+                  val timestamp: String,
+                  val iPinYouID: String,
+                  val userAgent: String,
+                  val ipAddress: String,
+                  val region: String,
+                  val city: String,
+                  val adExchange: String,
+                  val domain: String,
+                  val url: String,
+                  val anonymousURLID: String,
+                  val adSlotID: String,
+                  val adSlotWidth: String,
+                  val adSlotHeight: String,
+                  val adSlotVisibility: String,
+                  val adSlotFormat: String,
+                  val adSlotFloorPrice: String,
+                  val creativeID: String,
+                  val biddingPrice: Int,
+                  val advertiserId: String,
+                  val userTags: String
+                )
 
 object BidRequest{
 
