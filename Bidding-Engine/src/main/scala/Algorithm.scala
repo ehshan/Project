45cd9e1c4dc35dd779efc0f31ebe977268f14209
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-25 18:19:21 +0100

    updated algorithm

--- Bidding-Engine/src/main/scala/Algorithm.scala
+++ Bidding-Engine/src/main/scala/Algorithm.scala
@@ -1,4 +1,21 @@
 
-class Algorithm {
+sealed trait Algorithim {
+
+  def init()
+
+  def getBidPrice(bidRequest: BidRequest): Int
+
+}
+
+class Bid extends Algorithim {
+
+
+  def init(){
+
+  }
+
+  def getBidPrice(bidRequest: BidRequest): Int = {
+    1
+  }
 
 }
