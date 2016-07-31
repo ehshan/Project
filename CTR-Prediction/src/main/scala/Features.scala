fd6474cc35e3cb6c313021a55749cab0b7ae03eb
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-31 17:25:48 +0100

    moved time extraction method to features

--- CTR-Prediction/src/main/scala/Features.scala
+++ CTR-Prediction/src/main/scala/Features.scala
@@ -1,7 +1,42 @@
+import java.util.Calendar
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
 /**
   * Object to extract second order data features
+  *
   * @author Ehshan-Veerabangsa
   */
 object Features {
 
+  def transformTime(df: DataFrame): DataFrame = {
+    //transformation functions
+    val getYear: (String => Int) = (arg: String) => convertTime(arg, Calendar.YEAR)
+    val getMonth: (String => Int) = (arg: String) => convertTime(arg, Calendar.MONTH)
+    val getDay: (String => Int) = (arg: String) => convertTime(arg, Calendar.DAY_OF_MONTH)
+    val getHour: (String => Int) = (arg: String) => convertTime(arg, Calendar.HOUR_OF_DAY)
+
+    val yearFunc = udf(getYear)
+    val monthFunc = udf(getMonth)
+    val dayFunc = udf(getDay)
+    val hourFunc = udf(getHour)
+
+    //adding new columns with new time features
+    df.withColumn("Year", yearFunc(df("Timestamp"))).withColumn("Month", monthFunc(df("Timestamp")))
+      .withColumn("Day", dayFunc(df("Timestamp"))).withColumn("Hour", hourFunc(df("Timestamp")))
+      .drop("Timestamp").drop("Year").cache() //dropping original timestamp feature//years has only one value
+  }
+
+  /**
+    * Helper method to convert timestamps
+    */
+  def convertTime(string: String, field: Int): Int = {
+    // getting the date format from new object parsing
+    val date = DateFormatter.formatter.get().parse(string)
+    //getting a calendar instance
+    val calendar = Calendar.getInstance()
+    calendar.setTime(date)
+    calendar.get(field)
+  }
 }
