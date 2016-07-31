00245d9a528c40eeffef916806c7cee498c1f4d8
Ehshan Veerabangsa <ehshan@msn.com>
2016-07-31 17:55:56 +0100

    moved timeOfDay method to features

--- CTR-Prediction/src/main/scala/Features.scala
+++ CTR-Prediction/src/main/scala/Features.scala
@@ -40,6 +40,22 @@ object Features {
     calendar.setTime(date)
     calendar.get(field)
   }
+
+  def timeOfDay(df: DataFrame): DataFrame = {
+    def time(td: Int): String = {
+      td match {
+        case x if 0 until 7 contains x => "midnight"
+        case x if 7 until 12 contains x => "morning"
+        case x if 12 until 16 contains x => "afternoon"
+        case x if 16 until 20 contains x => "evening"
+        case x if 20 until 23 contains x => "night"
+        case x => null
+      }
+    }
+    val tod: (Int => String) = (arg: Int) => time(arg)
+    val timeFunc = udf(tod)
+    df.withColumn("TimeOfDay", timeFunc(df("Hour"))).cache()
+  }
 }
 /**
   * Object to override the initial date format for a calendar object
