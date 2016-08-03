586f1284d20030d890f5c3e4c235f46f1ba687cf
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-03 18:36:11 +0100

    moved method for column creation form ml

--- CTR-Prediction/src/main/scala/ModelData.scala
+++ CTR-Prediction/src/main/scala/ModelData.scala
@@ -3,6 +3,20 @@
   */
 object ModelData {
 
+  /**
+    * Makes a index column for string-indexer
+    *
+    * @param col
+    * @return
+    */
+  def makeIndexColumn(col: String) = col + "-index"
 
+  /**
+    * Makes a vector column for one-hot-encoder
+    *
+    * @param col
+    * @return
+    */
+  def makeVectorColumn(col: String) = col + "-vector"
 
 }
