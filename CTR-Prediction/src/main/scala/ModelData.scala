91d183be704ca009d8766aad3beb6d489d7e9986
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-03 18:38:07 +0100

    moved method that creates a vector of binary features

--- CTR-Prediction/src/main/scala/ModelData.scala
+++ CTR-Prediction/src/main/scala/ModelData.scala
@@ -1,7 +1,35 @@
+import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
+import org.apache.spark.sql.DataFrame
+
 /**
   * Object to transform data for prediction models
   */
 object ModelData {
+
+  /**
+    * Method to encode a single passed to column of labeled indices & vector column of indices
+    *
+    * @param df
+    * @param column
+    * @return
+    */
+  def singleBinaryFeature(df: DataFrame,column: String): DataFrame = {
+    val labelIndexer = new StringIndexer()
+      .setInputCol(column)
+      .setOutputCol(makeIndexColumn(column))
+      .fit(df)
+      .transform(df)
+
+
+    val encoder = new OneHotEncoder()
+      .setDropLast(false)
+      .setInputCol(makeIndexColumn(column))
+      .setOutputCol(makeVectorColumn(column))
+
+    encoder.transform(labelIndexer)
+      .drop(column)
+      .drop(makeIndexColumn(column))
+  }
 
   /**
     * Makes a index column for string-indexer
