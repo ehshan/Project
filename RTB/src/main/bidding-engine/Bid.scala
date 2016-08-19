7dda89179a6ad2ba74f4801e2492f3dab45a23fd
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-19 20:15:07 +0100

    added base variables

--- RTB/src/main/bidding-engine/Bid.scala
+++ RTB/src/main/bidding-engine/Bid.scala
@@ -6,6 +6,14 @@ sealed trait Algorithim {
 
 class Bid extends Algorithim{
 
+  //THE AVERAGE BIDDING PRICE FROM TRAINING DATA
+  val baseBid = 88
+
+  //THE AVERAGE CTR FROM TRAINING DATA
+  val baseCTR = 8.237307135252534E-4
+
+  val fixedBidPrice = 100
+
 
   def getBidPrice(bidRequest: BidRequest):Int={
 
