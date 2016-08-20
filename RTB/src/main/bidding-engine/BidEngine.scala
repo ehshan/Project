51c8acbb8a60a815a7be88dcb2dd81848e103e8f
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-20 16:51:21 +0100

    added run session - method to stream in test- data and eval impression one-by-one

--- RTB/src/main/bidding-engine/BidEngine.scala
+++ RTB/src/main/bidding-engine/BidEngine.scala
@@ -1,7 +1,27 @@
+import java.io.{FileInputStream, BufferedInputStream}
+
+import scala.io.Source
+
 /**
   * Created by Ehshan on 20/08/2016.
   */
 object BidEngine {
+
+  //SESSION-3
+  val path = "D:\\_MSC_PROJECT\\sample-datasets\\i-pin-you-season-3\\leaderboard.test.data.20131021_28.txt"
+
+
+  /**
+    *
+    * @return
+    */
+  def runSession(): Iterator[BidRequest] ={
+    val logs =  Source.fromInputStream (new BufferedInputStream(new FileInputStream(path)))
+    val request = (line: String) => handleRequest(line)
+    val itt = logs.getLines().map(request)
+    itt
+
+  }
 
   /**
     * Returns the Bid Request Object for winning bid only
@@ -11,8 +31,6 @@ object BidEngine {
     */
   def handleRequest(s: String): BidRequest={
     val request = BidRequest(s)
-
-    val bid = new Bid
 
     request
 
