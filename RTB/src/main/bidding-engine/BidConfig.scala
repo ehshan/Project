2e792e922b15b3dee02582cdd191bc13f87243bd
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-24 19:41:24 +0100

    updated with global variables = path and features

--- RTB/src/main/bidding-engine/BidConfig.scala
+++ RTB/src/main/bidding-engine/BidConfig.scala
@@ -3,4 +3,10 @@
   */
 object BidConfig {
 
+  val features = Array("region","city","adExchange","adSlotWidth","adSlotHeight","adSlotVisibility",
+    "adSlotFormat","creativeID")
+
+  val path = "F:\\_MSC_PROJECT\\project-data\\bid-config\\"
+
+
 }
