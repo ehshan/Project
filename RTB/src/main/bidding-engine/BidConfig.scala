334e05cc90c45a3494b5dc335babbdc9e7bebe2d
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-25 19:13:08 +0100

    added function to load config values into map

--- RTB/src/main/bidding-engine/BidConfig.scala
+++ RTB/src/main/bidding-engine/BidConfig.scala
@@ -1,3 +1,7 @@
+import java.io.{FileInputStream, BufferedInputStream}
+
+import scala.io.Source
+
 /**
   * Created by Ehshan on 24/08/2016.
   */
@@ -7,6 +11,26 @@ object BidConfig {
     "adSlotFormat","creativeID")
 
   val path = "F:\\_MSC_PROJECT\\project-data\\bid-config\\"
+
+  /**
+    * Method to take a feature and get a map of all possible CTR values from config file and place to map
+    *
+    * @param feature
+    * @return
+    */
+  def configFeature(feature: String): Map[String, Double] ={
+
+    //DATABRICKS CVS WRITE IN PART FORMAT
+    val p = path+feature+"\\part-00000"
+
+    val data = Source.fromInputStream (new BufferedInputStream(new FileInputStream(p)))
+
+    data.getLines.map {
+      line => val Array(key,value) = line.split(',')
+        val double = value.toFloat.toDouble
+        key -> double }.toMap
+
+  }
 
 
 }
