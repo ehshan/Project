29964146321ffcdfde4690d538f6ceef7776b00a
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-27 17:19:26 +0100

    added paths

--- RTB/src/main/bidding-engine/BidFrame.scala
+++ RTB/src/main/bidding-engine/BidFrame.scala
@@ -1,3 +1,7 @@
 object BidFrame {
 
+  val path3 = "D:\\_MSC_PROJECT\\sample-datasets\\i-pin-you-season-3\\leaderboard.test.data.20131021_28.txt"
+
+  val path2 = "D:\\_MSC_PROJECT\\sample-datasets\\i-pin-you-season-2\\leaderboard.test.data.20130613_15.txt"
+
 }
