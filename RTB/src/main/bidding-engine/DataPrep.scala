0ede694575d0f0f39ae28095535c4002aafe9544
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-27 15:39:15 +0100

    moved split probabilty method from ml to data prep

--- RTB/src/main/bidding-engine/DataPrep.scala
+++ RTB/src/main/bidding-engine/DataPrep.scala
@@ -1,3 +1,25 @@
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
 object DataPrep {
+
+  /**
+    * Method to split probability vector from prediction to click and no-click columns
+    *
+    * @param df
+    * @return
+    */
+  def splitProbability(df: DataFrame): DataFrame ={
+    //SPLITS THE PROBABILITY TO CLICK/NO-CLICK PROBABILITIES
+    val no: (Vector => (Double)) = (arg: Vector) => arg(0)
+    val yes:(Vector => (Double)) = (arg: Vector) => arg(1)
+
+    val noClickProb = udf(no)
+    val clickProb = udf(yes)
+
+    df.withColumn("No-Click-Probability", noClickProb(df("Probability")))
+      .withColumn("Month", clickProb(df("Probability")))
+  }
 
 }
