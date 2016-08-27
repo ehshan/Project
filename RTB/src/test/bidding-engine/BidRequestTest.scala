4e1010a16b8ef71799650f0fa8c459b2af50544b
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-27 14:10:39 +0100

    updated test parameters

--- RTB/src/test/bidding-engine/BidRequestTest.scala
+++ RTB/src/test/bidding-engine/BidRequestTest.scala
@@ -4,28 +4,32 @@ import org.scalatest.FunSuite
 class BidRequestTest extends FunSuite{
 
   //A SAMPLE BID REQUEST
-  val line =
-    "88db9ffd2dc45e3f77fbffe53d01d5a5\t" +
-      "20131020100000045\t" +
-      "\t" +
-      "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)\t" +
-      "183.30.79.*\t" +
-      "216\t" +
-      "175\t" +
-      "1\t" +
-      "5777beaf611cab0f34afde6b9c3668c1\t" +
-      "f618c3cc3c0b547025a30a13bbbf0b8a\t" +
-      "null\t" +
-      "mm_17936709_2306571_9246546\t" +
-      "300\t" +
-      "250\t" +
-      "ThirdView\t" +
-      "Fixed\t" +
-      "0\t" +
-      "7323\t" +
-      "294\t" +
-      "2259\t" +
-      "null"
+  val line = "b8c557a16cdd9cea7fa61df79bdb392d\t" +
+    "20131022071300438\t" +
+    "1\t" +
+    "DABNdj1osJ4\t" +
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; QQBrowser/7.4.14929.400)\t" +
+    "4.216.169.*\t" +
+    "216\t" +
+    "233\t" +
+    "2\t" +
+    "5eaa6ffce5e827b0907e99679585db6b\t" +
+    "48fb2f0ea154912f4371d76461bc061b\t" +
+    "null\t" +
+    "1528566466\t" +
+    "468\t" +
+    "60\t" +
+    "OtherView\t" +
+    "Na\t" +
+    "5\t" +
+    "7328\t" +
+    "277\t" +
+    "230\t" +
+    "null\t" +
+    "2259\t" +
+    "10684,14273,10052,10133\t" +
+    "0\t" +
+    "0"
 
 
   test("Test BidRequest Constructor"){
