ccb817837bdcef078f2e0a06a2ebb4ecf48e8c35
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-28 17:19:05 +0100

    added test for feature transformation

---
+++ RTB/src/test/ctr-prediction/TransformTest.scala
@@ -0,0 +1,25 @@
+import org.scalatest.FunSuite
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+class TransformTest extends FunSuite{
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("test-data-frame").setMaster("local")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+
+    //PRE-PROCESS DATA
+    val df = Data.build(sc, sqlContext)
+
+    val transformed = Features.addSecondOrder(df)
+
+    //CHECK DATA
+    transformed.show()
+
+    //CHECK DATA FORMAT
+    transformed.printSchema()
+
+  }
+}
