9efef9017c88a91ca3c9645029ac256142a7a25e
Ehshan Veerabangsa <ehshan@msn.com>
2016-08-28 16:51:06 +0100

    updated test for data preprocess

--- RTB/src/test/ctr-prediction/DataTest.scala
+++ RTB/src/test/ctr-prediction/DataTest.scala
@@ -1,8 +1,24 @@
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
 import org.scalatest.FunSuite
 
 
 class DataTest extends FunSuite{
 
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("test-data-frame").setMaster("local")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
 
+    //PRE-PROCESS DATA
+    val df = Data.build(sc, sqlContext)
+
+    //CHECK DATA
+    df.show()
+
+    //CHECK DATA FORMAT
+    df.printSchema()
+
+  }
 
 }
